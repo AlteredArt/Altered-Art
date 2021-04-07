ca3943e13796844be928643b191aac6177bfb8ba
Altered Art <cherokeedreemer@gmail.com>
2021-04-06 18:46:00 -0600

    work

---
+++ services.php
@@ -0,0 +1,6 @@
+
+<?php require('components/head.inc.php'); ?>
+<?php include('components/navbar.inc.php'); ?>
+<?php include('components/services.inc.php'); ?>
+<?php include('components/companies.inc.php'); ?>
+<?php require('components/footer.inc.php'); ?>
